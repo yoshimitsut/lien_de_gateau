610a7152a7efd69fff49e2fb7b1774fa4d76b01b
yoshimitsut <shimitsutanaka@gmail.com>
2026-02-18 20:23:35 -0300

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env['PORT'] || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API funcionando!'});
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <span>News letter</span>  
+    </>
+  )
+}
+
+export default App
